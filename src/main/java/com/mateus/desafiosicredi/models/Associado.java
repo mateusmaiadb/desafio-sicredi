9bb122d23ba1098a0b3494d420b676f172eddb7a
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-10-10 17:16:52 -0300

    feat: model de associado criada

---
+++ src/main/java/com/mateus/desafiosicredi/models/Associado.java
@@ -0,0 +1,26 @@
+package com.mateus.desafiosicredi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Associado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "associado")
+    private List<Voto> votos = new ArrayList<>();
+
+}
